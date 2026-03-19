1546f2dbc472231948e2490c7c0b3d66f76a02c3
Daniela Ene <danielaelena.08@gmail.com>
2026-03-19 23:46:46 +0000

    Add fullscreen hero background image to home

--- Zentrip-React/src/components/home/Home.jsx
+++ Zentrip-React/src/components/home/Home.jsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <>Home</>;
+  return (
+    <div
+      style={{
+        backgroundImage: "url('/img/background/home/hero/background_hero.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        width: "calc(100% + 2rem)",
+        margin: 0,
+        padding: 0,
+        marginLeft: "-1rem",
+        marginTop: "-6.5rem",
+      }}
+    />
+  );
 }
